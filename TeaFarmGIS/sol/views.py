0cabb9ad5e4162eeab1c4ff64a8ad8613c7c1611
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-07-12 23:55:27 +0300

    Updated views login system

--- TeaFarmGIS/sol/views.py
+++ TeaFarmGIS/sol/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import *
 from .serializers import *
-from rest_framework.permissions import AllowAny
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import filters
 
 
 from rest_framework import status
@@ -33,21 +34,47 @@ class UserViewSet(viewsets.ModelViewSet):
 
     
 #======================Creating viewsets==================================
-class FarmerViewSet(viewsets.ModelViewSet):
+class FarmerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Farmer.objects.all()
     serializer_class = FarmerSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name', 'phone']
 
 class FarmViewSet(viewsets.ModelViewSet):
     queryset = Farm.objects.all()
     serializer_class = FarmSerializer
+    permission_classes = [IsAuthenticated]
 
 class DailyTeaDeliveryViewSet(viewsets.ModelViewSet):
-    queryset = DailyTeaDelivery.objects.all()
+    queryset = DailyTeaDelivery.objects.all().select_related('farmer')
     serializer_class = DailyTeaDeliverySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['farmer', 'date']
+    search_fields = ['farmer__first_name', 'farmer__last_name']
+    ordering_fields = ['date', 'quantity_kg']
+    
+    def get_queryset(self):
+        # Only show deliveries for farmers the user is associated with
+        user = self.request.user
+        if user.groups.filter(name='Farmers').exists():
+            return DailyTeaDelivery.objects.filter(farmer__user=user)
+        return super().get_queryset()
+    
+    def perform_create(self, serializer):
+        # Automatically associate with farmer if user is a farmer
+        user = self.request.user
+        if user.groups.filter(name='Farmers').exists():
+            farmer = Farmer.objects.get(user=user)
+            serializer.save(farmer=farmer)
+        else:
+            serializer.save()
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
 
 class TeaCollectionCenterViewSet(viewsets.ModelViewSet):
     queryset = TeaCollectionCenter.objects.all()
