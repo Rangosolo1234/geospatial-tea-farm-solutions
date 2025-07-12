f68ace3cc2a4ad32c534c80cfc511bbc053a4951
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-07-12 23:59:22 +0300

    Included path to get login

--- TeaFarmGIS/sol/urls.py
+++ TeaFarmGIS/sol/urls.py
@@ -21,6 +21,7 @@ router.register(r'users', views.UserViewSet)
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('', include(router.urls)),
     path('register/', register_user),
 ]
