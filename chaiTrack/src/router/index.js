cefa6239d48d861b23591afc1b85ac084c1b3042
rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-08-04 23:18:27 +0300

    added more routes

--- chaiTrack/src/router/index.js
+++ chaiTrack/src/router/index.js
@@ -1,12 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Signup from '../components/Signup.vue'
-import Dashboard from '../components/Dashboard.vue'
+// import Dashboard from '../components/Dashboard.vue'
+import Expenses from '../components/Expenses.vue'
+import Deliveries from '../components/Deliveries.vue'
+import LandingPage from '../components/LandingPage.vue'
+import CentersMap from '../components/CentersMap.vue'
+import SoilData from '../components/SoilData.vue'
+import TruckLocationsMap from '../components/TruckLocationsMap.vue'
+import WeatherMap from '../components/WeatherMap.vue'
 
 const routes = [
-  { path: '/', component: Dashboard, meta: { requiresAuth: true } },
+  { path: '/', component: LandingPage },
   { path: '/login', component: Login },
   { path: '/signup', component: Signup },
+  { path: '/expenses', component: Expenses, meta: { requiresAuth: true } },
+  { path: '/deliveries', component: Deliveries, meta: { requiresAuth: true } },
+  { path: '/centers', component: CentersMap, meta: {requiresAuth: true} },
+  { path: '/soil', component: SoilData, meta: {requiresAuth: true} },
+  { path: '/loctruck', component: TruckLocationsMap, meta: {requiresAuth: true} },
+   { path: '/weather', component: WeatherMap, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
