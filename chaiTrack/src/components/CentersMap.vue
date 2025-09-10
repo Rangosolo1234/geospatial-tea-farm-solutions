49fdc68419b7b2d16cc733b79d788edb49076ab2
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-09-10 09:50:24 +0300

    These are newly added components making progress

---
+++ chaiTrack/src/components/CentersMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-xl font-semibold">Deliveries</h1>
+    <!-- ========================Loop through delivery data ======================== -->
+    <ul>
+      <li v-for="d in deliveries" :key="d.id">{{ d.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import api from '../services/axios'
+
+const deliveries = ref([])
+
+onMounted(async () => {
+  const res = await api.get('deliveries/')
+  deliveries.value = res.data
+})
+</script>
---
+++ chaiTrack/src/components/TruckLocationsMap.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>work</p>
+</template>
