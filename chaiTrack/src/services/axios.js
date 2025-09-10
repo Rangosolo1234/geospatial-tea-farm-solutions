b1af4b1bc8eb62a349c5a490c9dfaf28aaefa30a
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-09-10 09:38:06 +0300

    This is the path where new axios file was saved

--- chaiTrack/src/services/axios.js
+++ chaiTrack/src/services/axios.js
@@ -1,11 +1,15 @@
-// src/services/axios.js
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     // Add more headers here if needed, e.g., Authorization
-//   }
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/',
 })
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 export default api
